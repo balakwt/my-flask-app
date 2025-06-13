956f74ae093353fc4955e8a2718f5dc45e321434
kwtbala <balasubramaniharidass@gmail.com>
2025-06-13 17:20:24 +0300

    Fix indentation in app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return "Hello from Flask inside Docker!"
-
-    if __name__ == '__main__':
-            app.run(host='0.0.0.0', port=5000)
+    return "Hello from Flask inside Docker!"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
